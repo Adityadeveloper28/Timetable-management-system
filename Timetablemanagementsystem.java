f94da8d1612691fa4abb2e05c8a61989e1d94a13
Aditya singh <115808044+Adityadeveloper28@users.noreply.github.com>
2023-06-27 11:16:39 +0530

    Add files via upload
    everything is made using netbeans apache the files are meant to be in the source folder , for the project to work efficiently

---
+++ Timetablemanagementsystem.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package timetablemanagementsystem;
+
+/**
+ *
+ * @author sss
+ */
+public class Timetablemanagementsystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        homepage ob = new homepage();
+        ob.setVisible(true);
+    }
+    
+}
